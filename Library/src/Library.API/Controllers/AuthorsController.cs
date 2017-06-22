af420b811676af7d33be2296dfbae3de170010d4
IMamic23 <mamic.ivica@gmail.com>
2017-06-22 10:58:30 +0200

    First commit

---
+++ Library/src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,60 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Library.API.Entities;
+using Library.API.Models;
+using Library.API.Helpers;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {    
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository) => _libraryRepository = libraryRepository;
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+                var authorsFromRepo = _libraryRepository.GetAuthors();
+
+                var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
+                return Ok(authors);
+        }
+
+        [HttpGet("{id}", Name = "GetAuthor")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var authorFromRepo = _libraryRepository.GetAuthor(id);
+
+            if (authorFromRepo == null)
+                return NotFound();
+
+            var author = Mapper.Map<AuthorDto>(authorFromRepo);
+            return Ok(author);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthor([FromBody] AuthorForCreationDto author)
+        {
+            if (author == null)
+                return BadRequest();
+
+            var authorEntity = Mapper.Map<Author>(author);
+
+            _libraryRepository.AddAuthor(authorEntity);
+
+            if (!_libraryRepository.Save())
+                throw new Exception("Creating of author failed on save.");
+
+            var authorToReturn = Mapper.Map<AuthorDto>(authorEntity);
+
+            return CreatedAtRoute("GetAuthor", new {id = authorToReturn.Id}, authorToReturn);
+        }
+    }
+}
