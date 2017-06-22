374b1b94c95253d0125dd17e39695d47a2f469ae
IMamic23 <mamic.ivica@gmail.com>
2017-06-22 13:16:29 +0200

    UpdatingABook Endpoint

--- Library/src/Library.API/Startup.cs
+++ Library/src/Library.API/Startup.cs
@@ -88,6 +88,7 @@ namespace Library.API
                 cfg.CreateMap<AuthorForCreationDto, Author>();
 
                 cfg.CreateMap<BookForCreationDto, Book>();
+                cfg.CreateMap<BookForUpdateDto, Book>();
             });
 
 
