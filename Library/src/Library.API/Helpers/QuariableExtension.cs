103af1b5ebaed7cb7ad785c02078efc3319394c7
IMamic23 <mamic.ivica@gmail.com>
2017-06-27 00:28:45 +0200

    Sorting v1

---
+++ Library/src/Library.API/Helpers/QuariableExtension.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.API.Services;
+using System.Linq.Dynamic.Core;
+
+namespace Library.API.Helpers
+{
+    public static class QuariableExtension
+    {
+        public static IQueryable<T> ApplySort<T>(this IQueryable<T> source, string orderBy,
+            Dictionary<string, PropertyMappingValue> mappingDictionary)
+        {
+            if(source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if(mappingDictionary == null)
+                throw new ArgumentNullException(nameof(mappingDictionary));
+
+            if (string.IsNullOrWhiteSpace(orderBy))
+                return source;
+
+            // the orderBy string is separated with "," so we split it
+            var orderByAfterSplit = orderBy.Split(',');
+
+            // apply each orderby clause in reverse order - otherwise, the
+            // IQueryable will be ordered in the wrong order
+            foreach (var orderByClause in orderByAfterSplit.Reverse())
+            {
+                var trimmedOrderByClause = orderByClause.Trim();
+
+                var orderDescending = trimmedOrderByClause.EndsWith(" desc");
+
+                var indexOfFirstSpace = trimmedOrderByClause.IndexOf(" ", StringComparison.Ordinal);
+                // ReSharper disable once ComplexConditionExpression
+                var propertyName = indexOfFirstSpace == -1
+                    ? trimmedOrderByClause
+                    : trimmedOrderByClause.Remove(indexOfFirstSpace);
+
+                if (!mappingDictionary.ContainsKey(propertyName))
+                    throw new ArgumentException($"Key mapping for {propertyName} is missing.");
+
+                //get the property mapping value
+                var propertyMappingValue = mappingDictionary[propertyName];
+                if(propertyMappingValue == null)
+                    throw new ArgumentNullException("propertyMappingValue");
+
+                // Run through the property names in reverse
+                foreach (var destinationProperty in propertyMappingValue.DestinationProperties.Reverse())
+                {
+                    // revert sort order if necessary
+                    if (propertyMappingValue.Revert)
+                        orderDescending = !orderDescending;
+
+                    source = source.OrderBy(destinationProperty + (orderDescending ? " descending" : " ascending"));
+                }
+            }
+            return source;
+        }
+    }
+}
--- Library/src/Library.API/Startup.cs
+++ Library/src/Library.API/Startup.cs
@@ -67,6 +67,7 @@ namespace Library.API
                     implementationFactory.GetService<IActionContextAccessor>().ActionContext;
                 return new UrlHelper(actionContext);
             });
+            services.AddTransient<IPropertyMappingService, PropertyMappingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
